8b0e3c04bc5d7792712df2a06f19e7f3d3bfb61b
Daniel Chote <daniel@chote.com>
2015-04-19 14:14:48 -0400

    Initial project

---
+++ telehash/telehash.h
@@ -0,0 +1,19 @@
+//
+//  telehash.h
+//  telehash
+//
+//  Created by Daniel Chote on 4/19/15.
+//  Copyright (c) 2015 Daniel Chote. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for telehash.
+FOUNDATION_EXPORT double telehashVersionNumber;
+
+//! Project version string for telehash.
+FOUNDATION_EXPORT const unsigned char telehashVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <telehash/PublicHeader.h>
+
+
